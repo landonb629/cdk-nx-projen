fe88dd624a7dc9ad6d1680389b18bf0e6aafb760
landon babay <landon.babay@caylent.com>
2024-01-26 01:15:09 -0500

    feat: working on projen locally

--- .projenrc.ts
+++ .projenrc.ts
@@ -1,5 +1,6 @@
-import { typescript } from 'projen';
+import { awscdk, typescript } from 'projen';
 
+const cdkVersion = '2.123.0'
 const project = new typescript.TypeScriptProject({
   defaultReleaseBranch: 'main',
   name: '@test/root',
@@ -10,4 +11,21 @@ const project = new typescript.TypeScriptProject({
   // devDeps: [],             /* Build dependencies for this module. */
   // packageName: undefined,  /* The "name" in package.json. */
 });
+
+new awscdk.AwsCdkTypeScriptApp({
+  parent: project,
+  name: '@test/application-a',
+  outdir: './packages/application-a',
+  cdkVersion, 
+  defaultReleaseBranch: 'main',
+  deps: []
+})
+
+new typescript.TypeScriptProject({
+  parent: project,
+  name: '@test/environment-library',
+  outdir: './packages/environment-library',
+  defaultReleaseBranch: 'main',
+})
+
 project.synth();
